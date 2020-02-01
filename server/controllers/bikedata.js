d0d7cdb8860208cec5510462b25347a55761460d
Nisufferi <joonas.niskanen@edu.savonia.fi>
2020-02-01 19:34:40 +0200

    date ja temp apit

--- server/controllers/bikedata.js
+++ server/controllers/bikedata.js
@@ -47,3 +47,35 @@ exports.getSingleBike = async (req, res, next) => {
     });
   }
 };
+
+exports.getTemperature = async (req, res, next) => {
+  try {
+    const data = await Bike.find({}, 'Temperature');
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data: data
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: 'Error at server'
+    });
+  }
+};
+
+exports.getDates = async (req, res, next) => {
+  try {
+    const data = await Bike.find({}, 'Date');
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data: data
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: 'Error at server'
+    });
+  }
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getBikes, getSingleBike } = require('../controllers/bikedata');
+const { getBikes, getSingleBike, getTemperature, getDates } = require('../controllers/bikedata');
 
 router.route('/').get(getBikes);
 router.route('/bike').get(getSingleBike);
+router.route('/temperature').get(getTemperature);
+router.route('/dates').get(getDates);
 
 module.exports = router;
