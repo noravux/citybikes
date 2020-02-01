0485efd5ad13bc5c1824238d090ea47aee252c58
noravux <jiri.tukiainen@edu.savonia.fi>
2020-02-01 18:41:45 +0200

    tempdata taulukko

--- client/src/components/WeatherCard.vue
+++ client/src/components/WeatherCard.vue
@@ -23,18 +23,26 @@ export default {
 
   data() {
     return {
-      weatherData: undefined
+      weatherData: undefined,
+      tempData: []
     };
   },
 
   methods: {
     getWeather() {
+      //let tempData = [];
       let url =
         'https://api.openweathermap.org/data/2.5/forecast?id=650225&units=metric&appid=b448f0bf7189a64a46433a7b955951b3&cnt=13';
 
       this.$http.get(url).then(res => {
         this.setWeatherData(res.data.list);
-        console.log(res.data.list);
+
+        for (let i = 0; i < this.weatherData.length; i++) {
+          console.log(this.weatherData[i].main.temp);
+          this.tempData.push(this.weatherData[i].main.temp);
+        }
+
+        console.log('Tempdata: ' + this.tempData);
       });
     },
     setWeatherData(data) {
