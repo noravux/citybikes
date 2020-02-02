b780b73083ba608df0e2368b02cb0d9fbe096767
noravux <jiri.tukiainen@edu.savonia.fi>
2020-02-02 12:44:49 +0200

    Juh

---
+++ client/src/components/pages/ChartPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container>
+    <ChartComponent />
+  </b-container>
+</template>
+
+<script>
+import ChartComponent from '../Charttipaska';
+export default {
+  name: 'ChartPage',
+  components: {
+    ChartComponent
+  }
+};
+</script>
+
+<style></style>
