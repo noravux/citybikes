ad5b43c32e183fd73fb71e9df805ccfe6fba55a2
Nisufferi <joonas.niskanen@edu.savonia.fi>
2020-02-02 11:10:39 +0200

    arvioidut lämpötilojen keskiarvot

--- client/src/components/WeatherCard.vue
+++ client/src/components/WeatherCard.vue
@@ -14,7 +14,18 @@ export default {
       weatherData: undefined,
       tempData: [],
       hcTemperatures: [29,15,9,17,34],
-      hcRentals: []
+      hcRentals: [],
+      temp0_3 : 93,
+      temp3_6 :460,
+      temp6_9 : 607,
+      temp9_12 : 536,
+      temp12_15 : 620,
+      temp15_18 : 766,
+      temp18_21 : 832,
+      temp21_24 : 878,
+      temp24_27 : 1023,
+      temp27_30 : 1047
+
     };
   },
 
